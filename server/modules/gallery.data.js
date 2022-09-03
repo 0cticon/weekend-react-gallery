f80cfc5d901a747115c588402c3a2ca196ff9f7f
Kyle VanTassell <octicon@gmail.com>
2022-09-03 14:10:32 -0500

    added gallery data

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/firstFrenchOnion.jpeg', description: 'I had never had french onion soup, so I made some.', likes: 0 },
+    { id: 3, path: 'images/hotDog.jpeg', description: 'When we got Mighlough as a tiny puppy.', likes: 0 },
+    { id: 4, path: 'images/market.jpeg', description: 'One of the markets in Valencia, Spain. I would love to go back some day.', likes: 0},
+    { id: 5, path: 'images/oldHotDog.jpeg', description: 'Mighlough was a great pup, tons of fun. He made it to 13 years.', likes: 0},
+    { id: 6, path: 'public/images/fishyssoise.jpeg', desciption: 'Fish is an awesome cat, he and Mighlough became good friends.', likes: 0}
 ];
 
 module.exports = galleryItems;
